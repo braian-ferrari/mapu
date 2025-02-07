c3dffe5889abda551034913c7a46413654198a89
braian <braian.ferrari.bf@gmail.com>
2025-02-07 16:40:51 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      768: { slidesPerView: 2 },
+      1024: { slidesPerView: 3 },
+    },
+  });
+  
